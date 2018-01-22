3ab883be30e019337ef42be3483e2355e5ff9857
olafcortes <31994598+olafcortes@users.noreply.github.com>
2018-01-21 23:26:36 -0600

    drill 2 parte 3

---
+++ drill 2 (capitulo 3) parte 3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	string nombre;
+	string amigo;
+	cout << "hola usuario porfavor introdusca su nombre" << endl;
+	cin >> nombre;
+	cout << "hola "  << nombre << " Bienvenido a este programa "<< endl;
+	cout << "Disculpe " << nombre << " como se siente hoy?" << endl;
+	cout << nombre<< " Ya comio sus alimentos del desayuno?" << endl;
+	cout << "Yo digo que prepare carnita asada o no "<<nombre << endl;
+	cout << "la alarma de esta tarde sonara a las 6:30 quedo entendido? "<<nombre<<" ni-chan" << endl;
+	cout << "escribe el nombre de un amigo que tengas" << endl;
+	cin >> amigo;
+	cout << "Recuerda que tienes un pendiente con "<<amigo << endl;
+	cout << "Querido " << nombre << " lamentamos informarle que esta es la ultima linea de codigo por lo cual este finalizara cuando presiona enter " << endl;
+
+	cin.ignore();
+	cin.get();
+	return 0;
+}
