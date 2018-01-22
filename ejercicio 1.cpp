870433fdfbe7ee7c9fe23a6e4470861182cc3854
olafcortes <31994598+olafcortes@users.noreply.github.com>
2018-01-21 20:13:47 -0600

    ejerciccio numero 1

---
+++ ejercicio 1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include<vector> 
+#include<algorithm>
+#include<cmath>
+
+using namespace std;
+int main()
+{
+	cout << "Hello, programming " << endl;
+	cout << "Here we go " << endl;
+	return 0;
+}
