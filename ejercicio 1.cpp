194d3671526d0810e6edbb3c57ef678cd67ae5ec
olafcortes <31994598+olafcortes@users.noreply.github.com>
2018-01-21 20:38:44 -0600

    Update ejercicio 1.cpp

--- ejercicio 1.cpp
+++ ejercicio 1.cpp
@@ -9,5 +9,10 @@ int main()
 {
 	cout << "Hello, programming " << endl;
 	cout << "Here we go " << endl;
+	cout << "primero entro por la puerta principal y camino todo derecho " << endl;
+	cout << "luego giro a la izquierda para ir al pasillo"<< endl;
+	cout << "al caminar por el pasillo meterse por la primera puerta a la derecha y atravesar el patio " << endl;
+	cout << "despues de atravesar el patio meterse a la puerta y subir las escaleras " << endl;
+	cout << "al subir las escaleras abrir la puerta y girar a la izquierda para encntrar el bano y listo " << endl;
 	return 0;
 }
