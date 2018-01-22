1e4b00d18ce299b3a259b04f7047c0134e834fbb
olafcortes <31994598+olafcortes@users.noreply.github.com>
2018-01-21 21:42:40 -0600

    ejercicio 1 parte 4

---
+++ ejercicio 1 parte 4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	cout << "como preparar tacos" << endl;
+	cout << "primero obten los materiales " << endl;
+	cout << "lo que necesitas es carne, un sarten, una estufa y tortillas" << endl;
+	cout << "primero pon la carne en el sarten y luiego en la estufa a fuego lento" << endl;
+	cout << "espera mientras ordenas las tortillas" << endl;
+	cout << "una vez que la carne este lista cortala y distribuyela en las diferentes tortillas" << endl;
+	cout << "disfruta de los deliciosos tacos" << endl;
+	cin.get();
+	return 0;
+}
