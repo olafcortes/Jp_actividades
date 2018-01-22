7d5bf86bcf8230252d4eab74ba058b606157f215
olafcortes <31994598+olafcortes@users.noreply.github.com>
2018-01-21 20:12:00 -0600

    este es el drill 1

---
+++ drill 1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include<vector> 
+#include<algorithm>
+#include<cmath>
+
+using namespace std;
+int main()
+{
+	cout << "hello world!\n" << endl;
+	return 0;
+}
