c90673587f8b415dbf23e9c2476faccfbcc7e0f1
olafcortes <31994598+olafcortes@users.noreply.github.com>
2018-01-21 21:28:32 -0600

    ejercicio 1 numero 3

---
+++ ejercicio 1 parte 3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include<vector> 
+#include<algorithm>
+#include<cmath>
+
+using namespace std;
+int main()
+{
+	cout << "sacas el celular e inicias la aplicacion de uber " << endl;
+	cout << "despues pones el marcador en la ubicacion de la escuela o ingresas la direccion " << endl;
+	cout << "esperas tranquilamente el uber hasta que llegue " << endl;
+	cout << "una vez que lo veas procede a subirte " << endl;
+	cout << "disfruta el tranquilo viaje hasta llegar a tu destino " << endl;
+	cout << "das las gracias por el servicio que se te ofrecio y te bajas del carro " << endl;
+	cout << "entras a la escuela y te dirijes al salon en donde es tu clase " << endl;
+	return 0;
+}
